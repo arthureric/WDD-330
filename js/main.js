070b6f49ac80e474054e19cc7d5e3c0c3e14b891
arthureric <art21005@byui.edu>
2022-09-23 16:26:51 +0000

    commit

--- js/main.js
+++ js/main.js
@@ -1,67 +1,17 @@
-const links = [
-        {
-            label: "WEEK1",
-            url: "Week1/index.html"
-        },
-        {
-            label: "WEEK2",
-            url: "Week2/index.html"
-        },
-        {
-            label: "WEEK3",
-            url: "Week3/index.html"
-        },
-        {
-            label: "WEEK4",
-            url: "Week4/index.html"
-        },
-        {
-            label: "WEEK5",
-            url: "Week5/index.html"
-        },
-        {
-            label: "WEEK6",
-            url: "Week6/index.html"
-        },
-    
-        {
-            label: "WEEK7",
-            url: "Week7/index.html"
-        },
-    
-        {
-            label: "WEEK8",
-            url: "Week8/index.html"
-        },
-        {
-            label: "WEEK9",
-            url: "Week9/index.html"
-        },
-        {
-            label: "WEEK10",
-            url: "Week10/index.html"
-        },
-        {
-            label: "WEEK11",
-            url: "Week11/index.html"
-        },
-        {
-            label: "WEEK12",
-            url: "Week12/index.html"
-        }
-    ];
-    
-    
-    var contents = document.getElementById("contents");
-    
-    function generateContents() {
-        links.forEach(function (item, index) {
-            console.log(index, item);
-            let listItem = "<div><a href=\"" + item.url + "\" >" + item.label + "</a></div>";
-            console.log(listItem);
-            contents.innerHTML += listItem;
-        })
-    }
-    
-    
-    document.addEventListener("load", generateContents());
+const toggleMenu = () => {
+    document.querySelector('#navigation').classList.toggle('display');
+}
+
+document.querySelector('#navigation').addEventListener('click', toggleMenu);
+
+function sendAlert() {
+    alert("This is an alert() method!");
+}
+
+function scrollBottom() {
+    window.scrollTo(0,5000);
+}
+
+function scrollBack() {
+    window.scrollTo(0,50);
+}
