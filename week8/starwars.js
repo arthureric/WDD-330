99fc425d626c6d29fe2edbd065b7220fcefd6f35
arthureric <art21005@byui.edu>
2022-11-05 19:05:12 +0000

    commit

--- week8/starwars.js
+++ week8/starwars.js
@@ -1,110 +1,118 @@
+//Team1 W08 API Activity for Eric Arthur
+//Star Wars API - fetching and displaying pagination
 
-const apiPlanets = document.getElementById('planets');
-const apiPeople = document.getElementById('people');
-const apiFilms = document.getElementById('films');
-const outputDiv = document.getElementById('output');
-const apiShips = document.getElementById('ships');
-
-
-// the urls for each 
-// "films": "https://swapi.dev/api/films/",
-// "people": "https://swapi.dev/api/people/",
-// "planets": "https://swapi.dev/api/planets/",
-// "species": "https://swapi.dev/api/species/",
-const shipAPI = "https://swapi.dev/api/starships/";
-// "vehicles": "https://swapi.dev/api/vehicles/"
-const planetAPI = 'https://swapi.dev/api/planets/';
-const filmsAPI = 'https://swapi.dev/api/films/';
-const peopleAPI = 'https://swapi.dev/api/people/';
-
-
-
-//starwars api
-apiPlanets.addEventListener('click', () => {
-    fetch(planetAPI)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        outputDiv.innerHTML = '';
-        return response;
-    }
-    throw Error(response.statusText);
-    })
-    .then( response => response.json() )
-    .then( data => {
-        console.log(data);
-        for(let i = 0; i < 10; i++) {
-        outputDiv.innerHTML += 
-            `<h2>Planet: ${data.results[i].name}</h2>
-            <p>Rotation length: ${data.results[i].rotation_period}</p>
-            <p>Rotation length: ${data.results[i].orbital_period}</p>
-            <p>Climate: ${data.results[i].climate}</p>
-            <p>Diameter of planet: ${data.results[i].diameter}</p>
-            <p>Level of gravity: ${data.results[i].gravity}</p><br>
-            `
+/************ Model Code ******************
+ * Should handle anything to do with data *
+ * Usually connected to the database      *
+ * Responds to the Controller             *
+ ******************************************/
+/************* Fetch API data *************
+ * fetch returns string. json() method    *
+ * resolves promise as JS object          *
+ ******************************************/
+ function getJSON(url) {
+    return fetch(url)
+      .then((response) => {
+        parentDiv.innerText = "Waiting for response from server ...";
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          return response.json();
         }
-    })
-    .catch( error => console.log('There was an error:', error))
-},false);
-
-
-apiPeople.addEventListener('click', () => {
-    fetch(peopleAPI)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        outputDiv.innerHTML = '';
-        return response;
-    }
-    throw Error(response.statusText);
-    })
-    .then( response => response.json() )
-    .then (data => {
-        console.log(data);
-        for(let i = 0; i < 10; i++) {
-            outputDiv.innerHTML += 
-            `<h2>Name: ${data.results[i].name}</h2>
-            <p>Hair Color: ${data.results[i].hair_color}</p>
-            <p>Eye Color: ${data.results[i].eye_color}</p>
-            <p>Gender: ${data.results[i].gender}</p>
-            <p>Skin Color: ${data.results[i].skin_color}</p>
-            <p>Mass: ${data.results[i].mass}</p><br>
-            `
-        }
-    })
-        //shows the next page
-        //outputDiv.innerHTML = `${document.querySelector('a').setAttribute('href', data.next )}`}) 
-//      .then( data => {   
- 
-// })
-    .catch( error => console.log('There was an error:', error))
-},false);
-
-apiShips.addEventListener('click', () => {
-    fetch(shipAPI)
-    .then( response => {
-        outputDiv.innerHTML = 'Waiting for response...';
-    if(response.ok) {
-        outputDiv.innerHTML = '';
-        return response;
-    }
-    throw Error(response.statusText);
-    })
-    .then( response => response.json() )
-    .then (data => {
-        console.log(data);
-        document.querySelector("button.next").setAttribute('href', data.next);
-        for(let i = 0; i < 10; i++) {
-        outputDiv.innerHTML += 
-        `<h1>Ship Name: ${data.results[i].name}</h1>
-        <p>Model: ${data.results[i].model}</p>
-        <p>Manufacturer: ${data.results[i].manufacturer}</p>
-        <p>Crew size: ${data.results[i].crew}</p>
-        <p>Length of vessel: ${data.results[i].length}</p>
-        <p>Passengers: ${data.results[i].passengers}</p>
-        <p>Max atmosphering Speed: ${data.results[i].max_atmosphering_speed}</p>
-        `
-    }
-    })
-    .catch( error => console.log('There was an error:', error))
-},false);
+      })
+      .catch(function (error) {
+        console.log("Error: ", error);
+      });
+  }
+  
+  /************* Controller Code **************
+   * Interconnection between Model and View   *
+   * Handles no data logic.                   *
+   * Tells Model what to do. Takes info       *
+   * from Model, processes it, sends to View  *
+   ********************************************/
+  /**** Show People  ******/
+  function showPeople(url = "https://swapi.dev/api/people/") {
+    getJSON(url).then((data) => {
+      console.log({ data });
+      const people = data.results;
+      console.log({ people });
+      const peopleListElement = document.querySelector("#parentDiv");
+      const detailsContainer = document.querySelector("#detailsContainer");
+      console.log(peopleListElement);
+      renderPeopleList(people, peopleListElement);
+      if (data.next) {
+        const next = document.querySelector("#next");
+        next.ontouchend = () => {
+          showPeople(data.next);
+          detailsContainer.classList.add("hidden");
+        };
+        next.onclick = () => {
+          showPeople(data.next);
+          detailsContainer.classList.add("hidden");
+        };
+      }
+      if (data.previous) {
+        const previous = document.querySelector("#previous");
+        previous.ontouchend = () => {
+          showPeople(data.previous);
+          detailsContainer.classList.add("hidden");
+        };
+        previous.onclick = () => {
+          showPeople(data.previous);
+          detailsContainer.classList.add("hidden");
+        };
+      }
+    });
+  }
+  showPeople();
+  
+  /**** Person Details ****/
+  function showPersonDetails(person) {
+    const detailsContainer = document.querySelector("#detailsContainer");
+    detailsContainer.classList.remove("hidden");
+    renderPersonDetails(person);
+  }
+  
+  /*********** Views Code *********************
+   * Renders data representation .
+   * User Interface. Data collected by Model
+   * Processed by Controller. Controller
+   * sends all info to View to present it to UI
+   *********************************************/
+  
+  /***** Render People List *****/
+  function renderPeopleList(people, peopleListElement) {
+    const list = peopleListElement;
+    console.log({ list });
+    list.innerHTML = "<h2>Star Wars Characters</h2>";
+    people.forEach(function (person) {
+      console.log(person);
+      let listItem = document.createElement("p");
+      listItem.innerHTML = `<a href="${person.url}">${person.name}</a>`;
+      listItem.addEventListener("click", (e) => {
+        e.preventDefault();
+        showPersonDetails(person);
+      });
+      list.appendChild(listItem);
+    });
+  }
+  
+  /**** Render Person Details ****/
+  function renderPersonDetails(person) {
+    const elName = document.querySelector(".name");
+    const elGender = document.querySelector(".gender");
+    const elHeight = document.querySelector(".height");
+    const elMass = document.querySelector(".mass");
+    const elHairColor = document.querySelector(".hair_color");
+    const elEyeColor = document.querySelector(".eye_color");
+    const elSkinColor = document.querySelector(".skin_color");
+  
+    elName.innerText = person.name;
+    elGender.innerText = person.gender;
+    elHeight.innerText = person.height;
+    elMass.innerText = person.mass;
+    elHairColor.innerText = person.hair_color;
+    elEyeColor.innerText = person.eye_color;
+    elSkinColor.innerText = person.skin_color;
+  }
